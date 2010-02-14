eb34b60d004ef783a937cc76888e930ef40fce88
Demur Rumed <junkmute@hotmail.com>
2010-02-14 17:36:56 +0000

    Beware of featuritis; I decided to add space/tab conversion

--- flip.c
+++ flip.c
@@ -2,35 +2,49 @@
 //Original at http://www-ccrma.stanford.edu/~craig/utility/flip/flip.cpp
 #include <stdio.h>
 int main(int argc,char**argv){
-	char*temp=tmpnam(0);
-	if(argc<3||(argv[1][1]!='u'&&argv[1][1]!='d'&&argv[1][1]!='t'&&argv[1][1]!='m')) puts("Convert newline format\n\n-u NIX\n -d DOS\n -m MAC\n -t ???");
+	argv[0]=tmpnam(0);
+	if(argv[1][0]=='-') argv[1]++;
+	if(argc<3) puts("Convert newline format\n\nu NIX\nd DOS\nm MAC\nt ???\nsn \\t->n spaces\ntn n spaces->\\t");
 	else for(int ch,i=2;i<argc;i++){
 		FILE*rame=fopen(argv[i],"rb");
 		if(!rame) printf("? %s\n",argv[i]);
-		else if(argv[1][1]=='t'){
+		else if(argv[1][0]=='t'){
 			printf("%s: ",argv[i]);
 			while((ch=getc(rame))!=0xa&&ch!=0xd&&ch!=EOF);
 			puts(ch==0xa?"NIX":ch==EOF?"???":(getc(rame)==0xa?"DOS":"MAC"));
 			fclose(rame);
 		}else{
-			FILE*wame=fopen(temp,"wb");
-			int ch0d=0;
+			FILE*wame=fopen(argv[0],"wb");
+			int ch0d=0,chsp='0';
 			while((ch=getc(rame))!=EOF){
-				if(argv[1][1]=='d'){
+				switch(argv[1][0]){
+				case'd':
 					if(ch==0xd||(ch==0xa&&!ch0d)){
 						putc(0xd,wame);
 						putc(0xa,wame);
 					}else if(ch!=0xa) putc(ch,wame);
-				}else if(argv[1][1]=='m'){
+				break;case'm':
 					if(ch!=0xa) putc(ch,wame);
 					else if(!ch0d) putc(0xd,wame);
-				}else if(ch!=0xa||!ch0d) putc(ch==0xd?0xa:ch,wame);
+				break;case'u':putc(ch==0xd?0xa:ch,wame);
+				break;case's':
+					if(ch!='\t') putc(ch,wame);
+					else for(chsp=argv[1][1];chsp>'0';chsp--) putc(' ',wame);
+				break;case't':
+					if(ch!=' '){
+						for(;chsp>'0';chsp--) putc(' ',wame);
+						putc(ch,wame);
+					}else if(++chsp==argv[1][1]){
+						chsp='0';
+						putc('\t',wame);
+					}
 				ch0d=ch==0xd;
+				}
 			}
 			fclose(wame);
 			fclose(rame);
 			remove(argv[i]);
-			rename(temp,argv[i]);
+			rename(argv[0],argv[i]);
 		}
 	}
 	return 0;
