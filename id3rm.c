7d00ced0a1f1e76d0ce2300080251db4e59e60f5
Demur Rumed <junkmute@hotmail.com>
2010-03-04 12:20:18 +0000

    Remove temp file, use buffer instead. Next step: Merge prunes

--- id3rm.c
+++ id3rm.c
@@ -10,11 +10,10 @@
 	#define ID3 0x00334449
 	#define LEN(x) (((x&0x7F000000)>>24)|((x&0x7F0000)>>9)|((x&0x7F00)<<6)|((x&0x7F)<<21))
 #endif
-
+char temp[65536];
 int main(int argc,char**argv){
-	char*temp=tmpnam(0);
 	for(char**fn=argv+1;*fn;fn++){
-		FILE*f=fopen(*fn,"rb");
+		FILE*f=fopen(*fn,"rb+");
 		uint32_t buff=0;
 		if(!fseek(f,-128,SEEK_END)){
 			fread(&buff,3,1,f);
@@ -26,12 +25,14 @@ int main(int argc,char**argv){
 			fseek(f,3,SEEK_CUR);
 			fread(&buff,4,1,f);
 			fseek(f,LEN(buff),SEEK_CUR);
-			FILE*ft=fopen(temp,"wb");
-			for(int ch;(ch=fgetc(f))!=EOF;) fputc(ch,ft);
-			fclose(ft);
-			fclose(f);
-			remove(*fn);
-			rename(temp,*fn);
+			long rml=ftell(f),ffl=0,tpl;
+			do{
+				ffl+=tpl=fread(temp,1,sizeof(temp),f);
+				fseek(f,-rml-tpl,SEEK_CUR);
+				fwrite(temp,1,tpl,f);
+				fseek(f,rml,SEEK_CUR);
+			}while(tpl==sizeof(temp));
+			truncate(*fn,ffl);
 		}
 
 	}
