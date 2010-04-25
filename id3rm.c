216fe078d196b20c95d592b345361b3bf2d5e229
Demur Rumed <junkmute@hotmail.com>
2010-04-25 16:13:51 +0000

    aperm is a quickhack. id3rm took the easy way out

--- id3rm.c
+++ id3rm.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <stdint.h>
 #ifdef __BIG_ENDIAN__
@@ -11,7 +12,6 @@
 	#define LEN(x) (((x&0x7F000000)>>24)|((x&0x7F0000)>>9)|((x&0x7F00)<<6)|((x&0x7F)<<21))
 #endif
 int main(int argc,char**argv){
-	char temp[65536];
 	for(char**fn=argv+1;*fn;fn++){
 		FILE*f=fopen(*fn,"rb+");
 		uint32_t buff=0;
@@ -21,18 +21,18 @@ int main(int argc,char**argv){
 			fread(&buff,4,1,f);
 			fseek(f,LEN(buff),SEEK_CUR);
 			long rml=ftell(f);
-			do{
-				buff=fread(temp,1,sizeof(temp),f);
-				fseek(f,-rml-buff,SEEK_CUR);
-				fwrite(temp,1,buff,f);
-				fseek(f,rml,SEEK_CUR);
-			}while(buff==sizeof(temp));
+			fseek(f,0,SEEK_END);
+			long fln=ftell(f)-rml;
+			char*temp=malloc(fln);
+			fseek(f,rml,SEEK_BEGIN);
+			fread(temp,fnl,1,f);
+			frewind(f);
+			fwrite(temp,fln,1,f);
 			fseek(f,-128,SEEK_CUR);
 		}else fseek(f,-128,SEEK_END);
 		buff=0;
 		fread(&buff,3,1,f);
-		long a=ftell(f);
+		truncate(*fn,ftell(f)+(buff==TAG?-3:125));
 		fclose(f);
-		truncate(*fn,a+(buff==TAG?-3:125));
 	}
 }
