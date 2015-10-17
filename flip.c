e1f372ec9d949db36c636b4f0ae5458d92169a4f
Demur Rumed <gunkmute@gmail.com>
2015-10-17 01:54:21 +0000

    rm unlicense xlametry, revamp flip to malloc over temps, tested that it now works, also build.ninja for flip & id3rm

--- flip.c
+++ flip.c
@@ -1,53 +1,67 @@
 //Original http://www-ccrma.stanford.edu/~craig/utility/flip/flip.cpp
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 int main(int argc,char**argv){
-	char*argv1,*temp=tmpnam(0);
 	if(argc<3)puts("u NIX\nd DOS\nm MAC\n? ???\nsn \\t->n spaces\ntn n spaces->\\t");
-	else for(int ch,i=(argv1=argv[1]+(argv[1][0]=='-'),2);i<argc;i++){
+	const char*argv1=argv[1]+(argv[1][0]=='-');
+	if(!*argv1)return 0;
+	const char a0=*argv1,a1=argv1[1]-48;
+	if(a0!='?'&&a0!='u'&&a0!='s'&&a0!='t'&&a0!='d'&&a0!='m')return 0;
+	else for(int ch,i=2;i<argc;i++){
 		FILE*rame=fopen(argv[i],"rb");
 		if(!rame)printf("? %s\n",argv[i]);
-		else if(argv1[0]=='?'){
+		else if(a0=='?'){
 			while((ch=getc(rame))!=0xa&&ch!=0xd&&ch!=EOF);
 			printf("%s: %s\n",argv[i],ch==0xa?"NIX":ch==EOF?"???":(getc(rame)==0xa?"DOS":"MAC"));
 			fclose(rame);
-		}else if(argv1[0]=='d'||argv1[0]=='m'||argv1[0]=='u'||argv1[0]=='s'||argv1[0]=='t'){
-			FILE*wame=fopen(temp,"wb+");
+		}else{
+			fseek(rame,0,SEEK_END);
+			long rlen=ftell(rame);
+			char *tmp=malloc(rlen),*tp=tmp;
+			fseek(rame,0,SEEK_SET);
 			int ch0d=0;
 			while((ch=getc(rame))!=EOF){
-				switch(argv1[0]){
+				switch(a0){
 				case'd':
 					if(ch==0xd||(ch==0xa&&!ch0d)){
-						putc(0xd,wame);
-						putc(0xa,wame);
-					}else if(ch!=0xa)putc(ch,wame);
+						*tp++=0xd;
+						*tp++=0xa;
+					}else if(ch!=0xa)*tp++=ch;
 				break;case'm':
-					if(ch!=0xa)putc(ch,wame);
-					else if(!ch0d)putc(0xd,wame);
+					if(ch!=0xa)*tp++=ch;
+					else if(!ch0d)*tp++=0xd;
 				break;case'u':
-					if(!ch0d)putc(ch==0xd?0xa:ch,wame);
+					if(!ch0d)*tp++=(ch==0xd?0xa:ch);
+					else if (ch==0xd)*tp++=0xa;
 				break;case's':
-					if(ch!='\t')putc(ch,wame);
-					else for(ch0d=argv1[1]-48;ch0d;ch0d--)putc(' ',wame);
+					if(ch!='\t')*tp++=ch;
+					else{
+						memset(tp,' ',a1);
+						tp+=a1;
+					}
 				continue;case't':
 					if(ch!=' '){
-						for(;ch0d;ch0d--)putc(' ',wame);
-						putc(ch,wame);
-					}else if(++ch0d==(argv1[1]-48)){
+						if (ch0d){
+							memset(tp,' ',ch0d);
+							tp+=ch0d;
+							ch0d=0;
+						}
+						*tp++=ch;
+					}else if(++ch0d==a1){
 						ch0d=0;
-						putc('\t',wame);
+						*tp++='\t';
 					}
 					continue;
+				default:__builtin_unreachable();
 				}
 				ch0d=ch==0xd;
 			}
-			char a[ftell(wame)];
-			rewind(wame);
-			fread(a,1,sizeof(a),wame);
-			fclose(wame);
 			fclose(rame);
-			fopen(rame,"wb");
-			fwrite(a,1,sizeof(a),rame);
+			rame=fopen(argv[i],"wb");
+			fwrite(tmp,1,tp-tmp,rame);
 			fclose(rame);
+			free(tmp);
 		}
 	}
 }
